2c62ea1066e33a2acde9565f506f9971547e672e
Fabrice Devaux <fabrice.devaux@gmail.com>
2024-09-22 13:11:51 +0200

    Add SUV Coupe BodyType
    Fixes skodaconnect/homeassistant-myskoda#18

--- tests/test_rest_api.py
+++ tests/test_rest_api.py
@@ -13,32 +13,37 @@ FIXTURES_DIR = Path(__file__).parent.joinpath("fixtures")
 print(f"__file__ = {__file__}")
 
 
-@pytest.fixture(name="vehicle_info")
-def load_vehicle_info() -> str:
+@pytest.fixture(name="vehicle_infos")
+def load_vehicle_info() -> list[str]:
     """Load vehicle-info fixture."""
-    with FIXTURES_DIR.joinpath("enyaq/garage_vehicles_iv80.json").open() as file:
-        return file.read()
+    vehicle_infos = []
+    # TODO @dvx76: probably just glob all files
+    for path in ["enyaq/garage_vehicles_iv80.json", "enyaq/garage_vehicles_iv80_coupe.json"]:
+        with FIXTURES_DIR.joinpath(path).open() as file:
+            vehicle_infos.append(file.read())
+    return vehicle_infos
 
 
 @pytest.mark.asyncio
-async def test_get_info(vehicle_info: str) -> None:
+async def test_get_info(vehicle_infos: list[str]) -> None:
     """Example unit test for RestAPI.get_info(). Needs more work."""
-    vehicle_info_json = json.loads(vehicle_info)
-    response_mock = AsyncMock()
-    response_mock.text.return_value = vehicle_info
-    session_mock = MagicMock()
-    session_mock.get.return_value.__aenter__.return_value = response_mock
+    for vehicle_info in vehicle_infos:
+        vehicle_info_json = json.loads(vehicle_info)
+        response_mock = AsyncMock()
+        response_mock.text.return_value = vehicle_info
+        session_mock = MagicMock()
+        session_mock.get.return_value.__aenter__.return_value = response_mock
 
-    api = RestApi(session=session_mock)
-    api.idk_session = AsyncMock()
-    get_info_result = await api.get_info(vehicle_info_json["vin"])
+        api = RestApi(session=session_mock)
+        api.idk_session = AsyncMock()
+        get_info_result = await api.get_info(vehicle_info_json["vin"])
 
-    # Should probabaly assert the whole thing. Just an example.
-    assert get_info_result.name == vehicle_info_json["name"]
+        # Should probabaly assert the whole thing. Just an example.
+        assert get_info_result.name == vehicle_info_json["name"]
 
-    session_mock.get.assert_called_with(
-        "https://mysmob.api.connect.skoda-auto.cz/api/v2/garage/vehicles/TMBJM0CKV1N12345"
-        "?connectivityGenerations=MOD1&connectivityGenerations=MOD2&connectivityGenerations=MOD3"
-        "&connectivityGenerations=MOD4",
-        headers=ANY,
-    )
+        session_mock.get.assert_called_with(
+            "https://mysmob.api.connect.skoda-auto.cz/api/v2/garage/vehicles/TMBJM0CKV1N12345"
+            "?connectivityGenerations=MOD1&connectivityGenerations=MOD2&connectivityGenerations=MOD3"
+            "&connectivityGenerations=MOD4",
+            headers=ANY,
+        )
