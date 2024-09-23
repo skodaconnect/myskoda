789e307bdfddc9be8fe308890e144383322e834f
Dmitry Fursov <dmitri.foursov@gmail.com>
2024-09-23 20:16:25 +0300

    Add doors opened state for Superb
    Added test for vehicle status call based on the Superb iV responses.

--- tests/test_rest_api.py
+++ tests/test_rest_api.py
@@ -53,3 +53,44 @@ async def test_get_info(vehicle_infos: list[str]) -> None:
             "&connectivityGenerations=MOD4",
             headers=ANY,
         )
+
+
+@pytest.fixture(name="vehicle_statuses")
+def load_vehicle_status() -> list[str]:
+    """Load vehicle-status fixture."""
+    vehicle_statuses = []
+    for path in [
+        "superb/vehicle-status-doors-closed.json",
+        "superb/vehicle-status-right-front-door-opened.json",
+        "superb/vehicle-status-left-back-door-trunk-bonnet-opened.json",
+    ]:
+        with FIXTURES_DIR.joinpath(path).open() as file:
+            vehicle_statuses.append(file.read())
+    return vehicle_statuses
+
+
+@pytest.mark.asyncio
+async def test_get_status(vehicle_statuses: list[str]) -> None:
+    """Example unit test for RestAPI.get_status(). Needs more work."""
+    for vehicle_status in vehicle_statuses:
+        vehicle_status_json = json.loads(vehicle_status)
+        response_mock = AsyncMock()
+        response_mock.text.return_value = vehicle_status
+        session_mock = MagicMock()
+        session_mock.get.return_value.__aenter__.return_value = response_mock
+
+        authorization = Authorization(session_mock)
+        api = RestApi(session_mock, authorization)
+        api.authorization.get_access_token = AsyncMock()
+        target_vin = "TMBJM0CKV1N12345"
+        get_status_result = await api.get_status(target_vin)
+
+        assert get_status_result.overall.lights == vehicle_status_json["overall"]["lights"]
+        assert get_status_result.overall.doors == vehicle_status_json["overall"]["doors"]
+        assert get_status_result.detail.bonnet == vehicle_status_json["detail"]["bonnet"]
+        assert get_status_result.detail.trunk == vehicle_status_json["detail"]["trunk"]
+
+        session_mock.get.assert_called_with(
+            f"https://mysmob.api.connect.skoda-auto.cz/api/v2/vehicle-status/{target_vin}",
+            headers=ANY,
+        )
