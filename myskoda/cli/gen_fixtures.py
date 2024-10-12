1dba5887bbde92866fec52998d4c245645797d14
Frederick Gnodtke <frederick@gnodtke.net>
2024-10-12 20:49:36 +0200

    Add models for fixtures

--- myskoda/cli/gen_fixtures.py
+++ myskoda/cli/gen_fixtures.py
@@ -1,144 +1,41 @@
 """Generate a set of test fixtures from your garage."""
 
-import json
 from pathlib import Path
-from typing import Any
 
 import asyncclick as click
-import yaml
-from aioresponses import aioresponses
 from asyncclick.core import Context
 
-from myskoda.anonymize import anonymize_garage_entry
-from myskoda.const import BASE_URL_SKODA
-from myskoda.models.garage import GarageEntry
-from myskoda.models.info import CapabilityId
+from myskoda.models.fixtures import create_fixture_vehicle
 from myskoda.myskoda import MySkoda
 
 FIXTURES_DIR = Path(__file__).parent.parent.parent / "tests" / "fixtures" / "gen"
 
 
 @click.command()
-@click.option("vin", "--vin", help="Generate fixtures for this vin only.")
+@click.option(
+    "vehicle",
+    "--vehicle",
+    help="Generate fixtures for this vehicle only (vin). Set to 'all' for all vehicles.",
+    required=True,
+)
 @click.option("name", "--name", help="Short name describing the vehicle's state.", required=True)
-@click.option("description", "--description", help="A longer description.", required=True)
+@click.option("description", "--description", help="A longer description.")
 @click.pass_context
-async def gen_fixtures(ctx: Context, vin: str | None, name: str, description: str) -> None:
+async def gen_fixtures(ctx: Context, vehicle: str, name: str, description: str) -> None:
     """Interact with the MySkoda API."""
+    ctx.obj["name"] = name
+    ctx.obj["description"] = description
+
     myskoda: MySkoda = ctx.obj["myskoda"]
-    garage = await myskoda.get_garage()
 
-    if garage.vehicles is None:
-        return
+    vins = await get_vin_list(myskoda, vehicle)
+    vehicles = [create_fixture_vehicle(i, await myskoda.get_info(vin)) for i, vin in enumerate(vins)]
+    ctx.obj["vehicles"] = vehicles
 
-    for vehicle in garage.vehicles:
-        if vin is not None and vin != vehicle.vin:
-            continue
-
-        title = f"{vehicle.title.replace("\u0160koda", "").strip()} {vehicle.system_model_id}"
-        path = FIXTURES_DIR.joinpath(title.lower()).joinpath(name.lower())
-        path.mkdir(parents=True, exist_ok=True)
-
-        (path / "meta.yaml").write_text(
-            yaml.dump(
-                {
-                    "name": name,
-                    "description": description,
-                    "vehicle": anonymize_garage_entry(vehicle.to_dict()),
-                }
-            ),
-        )
-
-        await generate_fixtures(vehicle, myskoda, path)
+    print(vehicles)
 
 
-async def generate_fixture(url: str, raw: dict, data: Any, path: Path, vin: str) -> None:  # noqa: ANN401
-    with aioresponses() as mocked_responses:
-        raw_text = json.dumps(raw)
-        mocked_responses.get(url=f"{BASE_URL_SKODA}/api{url}", body=raw_text)
-        result = await data
-
-        path.mkdir(parents=True, exist_ok=True)
-        (path / "request.yaml").write_text(yaml.dump({"url": url.replace(vin, "TMOCKAA0AA000000")}))
-        (path / "parsed.yaml").write_text(yaml.dump(result.to_dict()))
-        (path / "raw.json").write_text(raw_text)
-
-
-async def generate_fixtures(vehicle: GarageEntry, myskoda: MySkoda, path: Path) -> None:
-    info = await myskoda.rest_api.get_info(vehicle.vin)
-    await generate_fixture(
-        url=myskoda.rest_api.get_info_url(vehicle.vin),
-        raw=json.loads(await myskoda.rest_api.get_info_raw(vehicle.vin, anonymize=True)),
-        data=myskoda.rest_api.get_info(vehicle.vin),
-        path=path / "info",
-        vin=vehicle.vin,
-    )
-    await generate_fixture(
-        url=myskoda.rest_api.get_maintenance_url(vehicle.vin),
-        raw=json.loads(await myskoda.rest_api.get_maintenance_raw(vehicle.vin, anonymize=True)),
-        data=myskoda.rest_api.get_maintenance(vehicle.vin),
-        path=path / "maintenance",
-        vin=vehicle.vin,
-    )
-    if info.is_capability_available(CapabilityId.CHARGING):
-        await generate_fixture(
-            url=myskoda.rest_api.get_charging_url(vehicle.vin),
-            raw=json.loads(await myskoda.rest_api.get_charging_raw(vehicle.vin, anonymize=True)),
-            data=myskoda.rest_api.get_charging(vehicle.vin),
-            path=path / "charging",
-            vin=vehicle.vin,
-        )
-    if info.is_capability_available(CapabilityId.STATE):
-        await generate_fixture(
-            url=myskoda.rest_api.get_status_url(vehicle.vin),
-            raw=json.loads(await myskoda.rest_api.get_status_raw(vehicle.vin, anonymize=True)),
-            data=myskoda.rest_api.get_status(vehicle.vin),
-            path=path / "status",
-            vin=vehicle.vin,
-        )
-    if info.is_capability_available(CapabilityId.AIR_CONDITIONING):
-        await generate_fixture(
-            url=myskoda.rest_api.get_air_conditioning_url(vehicle.vin),
-            raw=json.loads(
-                await myskoda.rest_api.get_air_conditioning_raw(vehicle.vin, anonymize=True)
-            ),
-            data=myskoda.rest_api.get_air_conditioning(vehicle.vin),
-            path=path / "air_conditioning",
-            vin=vehicle.vin,
-        )
-    if info.is_capability_available(CapabilityId.PARKING_POSITION):
-        await generate_fixture(
-            url=myskoda.rest_api.get_positions_url(vehicle.vin),
-            raw=json.loads(await myskoda.rest_api.get_positions_raw(vehicle.vin, anonymize=True)),
-            data=myskoda.rest_api.get_positions(vehicle.vin),
-            path=path / "positions",
-            vin=vehicle.vin,
-        )
-    if info.is_capability_available(CapabilityId.STATE):
-        await generate_fixture(
-            url=myskoda.rest_api.get_driving_range_url(vehicle.vin),
-            raw=json.loads(
-                await myskoda.rest_api.get_driving_range_raw(vehicle.vin, anonymize=True)
-            ),
-            data=myskoda.rest_api.get_driving_range(vehicle.vin),
-            path=path / "driving_range",
-            vin=vehicle.vin,
-        )
-    if info.is_capability_available(CapabilityId.TRIP_STATISTICS):
-        await generate_fixture(
-            url=myskoda.rest_api.get_trip_statistics_url(vehicle.vin),
-            raw=json.loads(
-                await myskoda.rest_api.get_trip_statistics_raw(vehicle.vin, anonymize=True)
-            ),
-            data=myskoda.rest_api.get_trip_statistics(vehicle.vin),
-            path=path / "trip_statistics",
-            vin=vehicle.vin,
-        )
-    if info.is_capability_available(CapabilityId.VEHICLE_HEALTH_INSPECTION):
-        await generate_fixture(
-            url=myskoda.rest_api.get_health_url(vehicle.vin),
-            raw=json.loads(await myskoda.rest_api.get_health_raw(vehicle.vin, anonymize=True)),
-            data=myskoda.rest_api.get_health(vehicle.vin),
-            path=path / "health",
-            vin=vehicle.vin,
-        )
+async def get_vin_list(myskoda: MySkoda, vehicle: str) -> list[str]:
+    if vehicle != "all":
+        return [vehicle]
+    return await myskoda.list_vehicle_vins()
