ab7842c9a19169debff45e8c417a1ba22df9feff
Nils Vogels <n.vogels@aves-it.nl>
2024-10-17 22:49:40 +0200

    Add fields from citigoe iv trip statistics

--- myskoda/models/trip_statistics.py
+++ myskoda/models/trip_statistics.py
@@ -11,6 +11,7 @@ from mashumaro.mixins.orjson import DataClassORJSONMixin
 class VehicleType(StrEnum):
     FUEL = "FUEL"
     HYBRID = "HYBRID"
+    ELECTRIC = "ELECTRIC"
 
 
 @dataclass
@@ -21,6 +22,15 @@ class StatisticsEntry(DataClassORJSONMixin):
     )
     average_speed_in_kmph: int | None = field(
         default=None, metadata=field_options(alias="averageSpeedInKmph")
+    )
+    average_electric_consumption: float | None = field(
+        default=None, metadata=field_options(alias="averageElectricConsumption")
+    )
+    average_recuperation: float | None = field(
+        default=None, metadata=field_options(alias="averageRecuperation")
+    )
+    average_aux_consumption: float | None = field(
+        default=None, metadata=field_options(alias="averageAuxConsumption")
     )
     mileage_in_km: int | None = field(default=None, metadata=field_options(alias="mileageInKm"))
     travel_time_in_min: int | None = field(
@@ -42,6 +52,9 @@ class TripStatistics(DataClassORJSONMixin):
     )
     overall_average_travel_time_in_min: int = field(
         metadata=field_options(alias="overallAverageTravelTimeInMin")
+    )
+    overall_average_electric_consumption: float | None = field(
+        default=None, metadata=field_options(alias="overallAverageElectricConsumption")
     )
     overall_mileage_in_km: int = field(metadata=field_options(alias="overallMileageInKm"))
     overall_travel_time_in_min: int = field(metadata=field_options(alias="overallTravelTimeInMin"))
