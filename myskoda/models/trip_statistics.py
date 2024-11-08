0a19d9563d463e3c578398d31b61cb7eba5a2720
WebSpider <WebSpider@users.noreply.github.com>
2024-11-08 04:29:53 +0100

    Make overall_average_speed_in_kmph optional (#168)

--- myskoda/models/trip_statistics.py
+++ myskoda/models/trip_statistics.py
@@ -41,9 +41,6 @@ class StatisticsEntry(DataClassORJSONMixin):
 
 @dataclass
 class TripStatistics(DataClassORJSONMixin):
-    overall_average_speed_in_kmph: int = field(
-        metadata=field_options(alias="overallAverageSpeedInKmph")
-    )
     overall_average_travel_time_in_min: int = field(
         metadata=field_options(alias="overallAverageTravelTimeInMin")
     )
@@ -61,4 +58,7 @@ class TripStatistics(DataClassORJSONMixin):
     )
     overall_average_mileage_in_km: int | None = field(
         default=None, metadata=field_options(alias="overallAverageMileageInKm")
+    )
+    overall_average_speed_in_kmph: int | None = field(
+        default=None, metadata=field_options(alias="overallAverageSpeedInKmph")
     )
