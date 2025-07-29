906699b588d3bafb8f00ad4063c17da10344ac0e
Nils Vogels <n.vogels@aves-it.nl>
2025-07-29 14:18:14 +0200

    feat: add models for new charging history

---
+++ myskoda/models/charging_history.py
@@ -0,0 +1,37 @@
+"""Models for responses of api/v1/charging/{vin}/history."""
+
+from dataclasses import dataclass, field
+from datetime import datetime
+from enum import StrEnum
+
+from mashumaro import field_options
+from mashumaro.mixins.orjson import DataClassORJSONMixin
+
+from .common import BaseResponse
+
+
+class ChargingCurrentType(StrEnum):
+    AC = "AC"
+    DC = "DC"
+
+
+@dataclass
+class ChargingSession(DataClassORJSONMixin):
+    start_at: datetime = field(metadata=field_options(alias="startAt"))
+    charged_in_kwh: float = field(metadata=field_options(alias="chargedInKwH"))
+    duration_in_minutes: int = field(metadata=field_options(alias="durationInMinutes"))
+    current_type: ChargingCurrentType = field(metadata=field_options(alias="currentType"))
+
+
+@dataclass
+class ChargingPeriod(DataClassORJSONMixin):
+    total_charged_in_kwh: float | None = field(
+        default=None, metadata=field_options(alias="totalChargedInKWh")
+    )
+    sessions: list[ChargingSession] | None = field(default=None)
+
+
+@dataclass
+class ChargingHistory(BaseResponse):
+    next_cursor: datetime | None = field(default=None, metadata=field_options(alias="nextCursor"))
+    periods: list[ChargingPeriod] | None = field(default=None)
