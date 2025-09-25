3648d4bc2160ad1587f3e342ea50226173821ee7
WebSpider <WebSpider@users.noreply.github.com>
2025-09-25 11:09:20 +0200

    fix: add chargingstate error (#480)

--- myskoda/models/charging.py
+++ myskoda/models/charging.py
@@ -47,6 +47,7 @@ class ChargingState(CaseInsensitiveStrEnum):
     CONNECT_CABLE = "CONNECT_CABLE"
     CONSERVING = "CONSERVING"
     CHARGING = "CHARGING"
+    ERROR = "ERROR"
 
 
 class ChargeType(StrEnum):
